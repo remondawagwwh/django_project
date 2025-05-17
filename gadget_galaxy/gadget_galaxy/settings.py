63e7633dc77e7d4183150932097d231676c25bf8
remondawagwwh <remondawageh88@gmail.com>
2025-05-18 02:51:27 +0300

    create and conection

--- gadget_galaxy/gadget_galaxy/settings.py
+++ gadget_galaxy/gadget_galaxy/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'gadget_galaxy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgres',
+        'NAME': 'mystore',
+        'USER': 'remonda',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
